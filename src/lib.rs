9fa455dc80cf81f63b4bd1247d58425c68a72490
Andrzej Broński <andrzej1_1@o2.pl>
2024-06-10 16:37:33 +0200

    COMMIT ALL THE FILES!

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+pub static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[cfg(target_arch = "wasm32")]
+#[panic_handler]
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    core::arch::wasm32::unreachable()
+}
